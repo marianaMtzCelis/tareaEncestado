2adf8c0823f68ba467f8b097ec9a5da06da1a4cf
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2020-03-30 22:18:40 -0600

    Working mouse manager

--- src/videogame/Game.java
+++ src/videogame/Game.java
@@ -58,6 +58,7 @@ public class Game implements Runnable {
         this.height = height;
         running = false;
         keyManager = new KeyManager();
+        mouseManager = new MouseManager();
     }
 
     /**
