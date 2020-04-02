568360f823f81e668fac7d1444ff82170f5279bc
diegomezcota <gomezcotadiego@gmail.com>
2020-04-01 18:04:32 -0700

    finished documentation

--- src/videogame/KeyManager.java
+++ src/videogame/KeyManager.java
@@ -14,14 +14,6 @@ import java.awt.event.KeyListener;
  */
 public class KeyManager implements KeyListener {
 
-    public boolean up;      // flag to move up the player
-    public boolean down;    // flag to move down the player
-    public boolean left;    // flag to move left the player
-    public boolean right;   // flag to move right the player
-    public boolean pause;   // the flag to pause the game
-    public boolean save;    // the flag to save the game
-    public boolean load;    // the flag to load the previous game from file
-
     private boolean keys[];  // to store all the flags for every key
 
     public KeyManager() {
@@ -43,25 +35,10 @@ public class KeyManager implements KeyListener {
         // set false to every key released
         keys[e.getKeyCode()] = false;
     }
-
-    public void releaseKey(int iCode) {
-        keys[iCode] = false;
-    }
-
-    public void releaseP() {
-        keys[KeyEvent.VK_P] = false;
-    }
-
+    
     /**
      * to enable or disable moves on every tick
      */
     public void tick() {
-        up = keys[KeyEvent.VK_UP];
-        down = keys[KeyEvent.VK_DOWN];
-        left = keys[KeyEvent.VK_LEFT];
-        right = keys[KeyEvent.VK_RIGHT];
-        pause = keys[KeyEvent.VK_P];
-        save = keys[KeyEvent.VK_G];
-        load = keys[KeyEvent.VK_C];
     }
 }
--- src/videogame/Player.java
+++ src/videogame/Player.java
@@ -14,19 +14,20 @@ import java.util.Date;
  */
 public class Player extends Item {
 
-    private int direction;
-    private Game game;
+    private int direction;          
+    private Game game;              // this game
     private Animation cookieAnimation; // stores the animation of player going up
-    private int vo, yo, xo;
-    private boolean isThrown;
-    private double t;
-    private static Date time = new Date();
+    private int vo, yo, xo;         // initial velocity and positions
+    private boolean isThrown;       // checks whether cookie has been throen
+    private double t;               // checks the time for the formula
+    private static Date time = new Date();  // current time
 
 
     public Player(int x, int y, int direction, int width, int height, Game game) {
         super(x, y, width, height);
         this.direction = direction;
         this.game = game;
+        // initial velocity
         vo = 0;
         t = new Date().getTime() - time.getTime() / 1000.0;
         isThrown = false;
@@ -89,68 +90,103 @@ public class Player extends Item {
         this.height = height;
     }
 
+    // updates x depending on conditions
     public void setX(int x){
+        // if on throwing area
         if (x <= 150 && x >= 0 && !isThrown){
             this.x = x;
         } else if (isThrown) {
+            // if it's in the air
             this.x = x;
         }
     }
     
+    // updates x depending on conditions
     public void setY(int y){
+        // if on throwing area
         if (y >= 0 && (y+80) <= game.getHeight() && !isThrown){
             this.y = y;
         } else if (isThrown) {
+            // if it's in the air
             this.y = y;
         }
     }
 
+    /**
+     * sets the isThrown state of the player
+     *
+     * @param isThrown
+     */
     public void setIsThrown(boolean isThrown) {
         this.isThrown = isThrown;
     }
 
+    /**
+     * gets the isThrown attribute of the player
+     *
+     * @return isThrown
+     */
     public boolean isIsThrown() {
         return isThrown;
     }
 
+    /**
+     * sets the t of the player
+     *
+     * @param t
+     */
     public void setT(double t) {
         this.t = t;
     }
 
+    /**
+     * gets the t of the player
+     *
+     * @return t
+     */
     public double getT() {
         return t;
     }
 
+    /**
+     * gets the time of the player
+     *
+     * @return time
+     */
     public static Date getTime() {
         return time;
     }
-
+    
+    /**
+     * sets the time of the player
+     *
+     * @param time
+     */
     public static void setTime(Date time) {
         Player.time = time;
     }
-    
-    
-    
-    
+
     @Override
     public void tick() {
+        // if it has not been thrown
         if (!isThrown) {
+            // move according to mouse
             setX(game.getMouseManager().getX());
             setY(game.getMouseManager().getY());
+            //  update xo and yo according to the position of the player until izquierdo is released
             if (!game.getMouseManager().isIzquierdo()) {
                 isThrown = true;
-//                vo = (185 - this.getX());
                 vo = (185 - this.getX());
                 xo = this.getX();
                 yo = this.getY();
-                //System.out.println(vo);
             } 
         } else {
+            // update position of cookie according to the formula
             for (int i = 0; i <= 1000; ++i){
                 setX(xo + (int)(vo*0.5253*t));
                 setY(yo - ((int)((vo*0.851*t) - (4.91*t*t))));
-                t = (new Date().getTime() - time.getTime()) / 1000.0 ;
-                //System.out.println(t);
+                t = (new Date().getTime() - time.getTime()) / 1000.0;
+                // tick cookie animation
                 this.cookieAnimation.tick();
             }
         }
@@ -158,11 +194,6 @@ public class Player extends Item {
 
     @Override
     public void render(Graphics g) {
-        
         g.drawImage(cookieAnimation.getCurrentFrame(), getX(), getY(), getWidth(), getHeight(), null);
-        
-        //g.drawImage(Assets.normalCookie, getX(), getY(), getWidth(), getHeight(), null);
-          
-     
     }
 }
